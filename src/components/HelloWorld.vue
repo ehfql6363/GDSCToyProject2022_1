8a9006ce158f11e5ca63bcbf258cd73f1f51aa20
김동열 <ehfql6363@naver.com>
2022-06-28 16:24:05 +0900

    20220628 메인페이지 무한 스크롤 구현 중
    Signed-off-by: 김동열 <ehfql6363@naver.com>

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,33 +15,46 @@
     </div>
     <div>
       <div>
-        <b-card
-          title="일정 제목"
-          img-src='@/assets/img/food.png'
-          img-alt="Image"
-          img-top
-          tag="article"
-          style="max-width: 20rem;"
-          class="mb-2"
-        >
-          <b-card-text>
-            일정 내용?
-          </b-card-text>
-
-          <b-button href="#" variant="primary">자세히 보기</b-button>
-        </b-card>
-</div>
+        <plan-observer v-on:triggerFadeIn="fadeIn">
+          <transition v-on:enter="enter">
+            <div v-if="show" ref="planWrapper" class="planWrapper">
+              <div v-for="(item, index) in items" :key="index">
+                {{item}}
+                <b-card
+                title="일정 제목"
+                img-src='@/assets/img/food.png'
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2">
+                  <b-card-text>일정 내용?</b-card-text>
+                  <b-button href="#" variant="primary">자세히 보기</b-button>
+                </b-card>
+              </div>
+            </div>
+          </transition>
+        </plan-observer>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import TriggerObserver from './TriggerObserver.vue'
+
 export default {
   name: 'HelloWorld',
+  components:{
+    PlanObserver: TriggerObserver
+  },
   data(){
     return{
       keyword: '',
-
+      show: false,
+      items:[
+        1,2,3,4,5,6,7,8,9,10
+      ]
     }
   },
   methods: {
@@ -60,7 +73,16 @@ export default {
         alert('검색어를 입력해주세요!')  //검색어를 입력하지 않은 경우
       }
     },
-  }
+    enter: function(el){
+      el.style.opacity = 0;
+    },
+    fadeIn: function(){
+      this.$refs.planWrapper.style = "transition: opacity 1s";
+    }
+  },
+  mounted() {
+    this.show = true;
+  },
 }
 </script>
 
---
+++ src/components/TriggerObserver.vue
@@ -0,0 +1,34 @@
+<template>
+  <div ref="triggerDiv">_</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      observer: null,
+      option: {
+        root: null,
+        threshold: 1,
+      },
+    };
+  },
+  methods: {
+    handleIntersect: function (target) {
+      if (target.isIntersecting) this.$emit(`triggerFadeIn`, this.OID);
+    },
+  },
+  mounted() {
+    this.observer = new IntersectionObserver((entries) => {
+      this.handleIntersect(entries[0]);
+    }, this.option);
+    this.observer.observe(this.$refs.triggerDiv);
+  },
+};
+</script>
+
+<style scoped>
+div {
+  opacity: 0;
+}
+</style>
